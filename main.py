6ca9cd5cd24d1999430d8bfec27b63a556335c3d
Stephen Yang <stepheny755@gmail.com>
2019-03-01 18:54:26 -0800

    updated main.py

---
+++ main.py
@@ -0,0 +1,36 @@
+import discord
+import asyncio
+import time
+import sys
+
+token = open('token.txt',"r").read().strip()
+
+client = discord.Client()
+
+@client.event
+async def on_read():
+    print('Logged in as')
+    print(client.user.name)
+    print(client.user.id)
+    print('------')
+
+
+@client.event
+async def on_message(message):
+    if message.content.startswith('!test'):
+        counter = 0
+        tmp = await client.send_message(message.channel, 'Calculating messages...')
+        async for log in client.logs_from(message.channel, limit=100):
+            if log.author == message.author:
+                counter += 1
+        await client.edit_message(tmp, 'You have {} messages.'.format(counter))
+
+    elif message.content.startswith('!sleep'):
+        await asyncio.sleep(5)
+        await client.send(message.channel, 'Done sleeping')
+
+    print(str(message.channel)+": "+message.content)
+
+if(__name__=="__main__"):
+    client.run(token)
+    print("Bot Started")
