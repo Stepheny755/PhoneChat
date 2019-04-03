1a3f039ac697569325aed82b2412da137e3e9767
Stephen Yang <stepheny755@gmail.com>
2019-04-02 23:21:05 -0700

    phone call function test

--- PhoneChat/out.py
+++ PhoneChat/out.py
@@ -1,17 +1,26 @@
 #SEND DATA TO CALLER
+
+from util import Util
 from twilio.rest import Client
 
 
 # Your Account Sid and Auth Token from twilio.com/console
 # DANGER! This is insecure. See http://twil.io/secure
-account_sid = open('sID.txt',"r").read().strip()
-auth_token = 'your_auth_token'
-client = Client(account_sid, auth_token)
 
-call = client.calls.create(
+class Out():
+
+    def __init__(self):
+        self.account_sid = open('sID.txt',"r").read().strip()
+        self.auth_token = open('token.txt',"r").read().strip()
+        self.client = Client(self.account_sid, self.auth_token)
+
+    def call(self,fr,to,url):
+        call = self.client.calls.create(
                         url='http://demo.twilio.com/docs/voice.xml',
-                        to='+15558675310',
-                        from_='+15017122661'
+                        to=str(to),
+                        from_=str(fr),
                     )
+        return call
 
-print(call.sid)
+o = Out()
+print(o.call("+17788006793","+17788914659","").sid)
